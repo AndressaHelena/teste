fd7fbb699d843f17134fc0d313444de1fda0ccdb
Andressa Helena <handressa125@gmail.com>
2022-07-06 10:01:37 -0300

    initial commit

---
+++ introJava/src/treinamento/com/tabuada.java
@@ -0,0 +1,16 @@
+package treinamento.com;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("");
+			for (int j =1; j <= 10; j++) {
+				System.out.println(j +"x" + i + " = "+ j * i);
+
+			}
+		}
+	}
+
+}
