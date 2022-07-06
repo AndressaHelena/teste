af123f7b30f48c34fd47533967ffeffb71a5b80f
Andressa Helena <handressa125@gmail.com>
2022-07-06 14:49:37 -0300

    desafios code girls

---
+++ introJava/src/CodeGirls/Intervalo.java
@@ -0,0 +1,28 @@
+package CodeGirls;
+
+import java.util.Scanner;
+
+public class Intervalo {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+	        double valor = scan.nextDouble();
+	      
+	        
+	        if (valor >= 0 && valor <= 25) {
+	            System.out.println("Intervalo [0,25]");
+	        } else if (valor > 25 && valor <= 50) {
+	            System.out.println("Intervalo (25,50]");
+	        } else if (valor > 50 && valor <= 75) {
+	            System.out.println("Intervalo (50,75]");  
+	        } else if (valor > 75 && valor <= 100) {
+	            System.out.println("Intervalo (75,100]");
+	        } else {
+	            System.out.println("Fora de intervalo");
+	        }
+	}  
+}
+
+
